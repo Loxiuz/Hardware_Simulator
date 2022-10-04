8a94aa2470a3bb818c63692b4963c2759888ad93
malte <maltebjoerklund@gmail.com>
2022-10-04 02:28:18 +0200

    Added commands to AST-Class.

--- AST.java
+++ AST.java
@@ -1,52 +1,89 @@
 public abstract class AST {}
 
-class Identifier {
+abstract class Expr extends AST{}
 
-    public String identifier;
+    class Identifier {
 
-    public Identifier(String identifier){
-        this.identifier = identifier;
+        public String id;
+
+        public Identifier(String id) {
+            this.id = id;
+        }
+
+        public String eval() {return id;}
     }
-    public String eval(){return identifier;}
-}
-class Const{
+    class Const {
 
-    public Integer c;
+        public Integer c;
 
-    public Const(Integer c){
-        this.c = c;
+        public Const(Integer c) {
+            this.c = c;
+        }
+
+        public Integer eval() {return c;}
     }
-    public Integer eval(){return c;}
-}
-/************************************************/
+/*--------------------------------------------------*/
 abstract class Command extends AST{
     abstract public boolean eval();
 }
+    class Hardware extends Command{
+    
+
+        @Override
+        public boolean eval() {return false;}
+    }
+    class Inputs extends Command{
 
 
+        @Override
+        public boolean eval() {return false;}
+    }
+    class Outputs extends Command{
 
-/************************************************/
+
+        @Override
+        public boolean eval() {return false;}
+    }
+    class Latch extends Command{
+
+
+        @Override
+        public boolean eval() {return false;}
+    }
+    class Update extends Command{
+
+
+        @Override
+        public boolean eval() {return false;}
+    }
+    class Simulate extends Command{
+
+
+        @Override
+        public boolean eval() {return false;}
+    }
+/*--------------------------------------------------*/
 abstract class Condition {
     abstract public boolean eval();
 }
-class And extends Condition{
+    class And extends Condition{
 
-    Condition c1;
-    Condition c2;
+        Condition c1;
+        Condition c2;
 
-    public And(Condition c1, Condition c2){
-        this.c1 = c1;
-        this.c2 = c2;
+        public And(Condition c1, Condition c2){
+            this.c1 = c1;
+            this.c2 = c2;
+        }
+        public boolean eval(){return c1.eval() && c2.eval();}
     }
-    public boolean eval(){return c1.eval() && c2.eval();}
-}
-class Negation extends Condition{
+    class Negation extends Condition{
 
-    Condition c1;
+        Condition c1;
 
-    public Negation(Condition c1){
-        this.c1 = c1;
+        public Negation(Condition c1){
+            this.c1 = c1;
+        }
+        public boolean eval(){return !c1.eval();}
     }
-    public boolean eval(){return !c1.eval();}
-}
-/************************************************/
+/*--------------------------------------------------*/
